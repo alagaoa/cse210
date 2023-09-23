32226f4545f8617d00e5fae03c79cb212ff06a69
alagaoa <alagaoarthur@gmail.com>
2023-09-22 17:39:47 -0700

    Added if statements

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -6,5 +6,42 @@ class Program
     {
         // This is prep 2
         Console.WriteLine("Hello Prep2 World!");
+
+        Console.Write("What is your grade percentage? ");
+        string gradePercentage = Console.ReadLine();
+        int grade = int.Parse(gradePercentage);
+        string gradeLetter = "S";
+
+        if (grade >= 90)
+        {
+            gradeLetter = "A";
+        }
+        else if (grade >= 80)
+        {
+            gradeLetter = "B";
+        }
+        else if (grade >= 70)
+        {
+            gradeLetter = "C";
+        }
+        else if (grade >= 60)
+        {
+            gradeLetter = "D";
+        }
+        else
+        {
+            gradeLetter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {gradeLetter}");
+
+        if (grade < 70)
+        {
+            Console.WriteLine("Better luck next time!");
+        }
+        else 
+        {
+            Console.WriteLine("Congrats you passed!");
+        }
     }
 }
