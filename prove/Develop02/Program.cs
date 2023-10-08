b1cebc1858894bc27185515beaff56e4157e2e12
alagaoa <alagaoarthur@gmail.com>
2023-10-07 21:09:51 -0700

    project

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,134 @@
 using System;
 
+class Entry
+{
+    public string _prompt { get; set; }
+    public string _response { get; set; }
+    public DateTime _date { get; set; }
+
+    public Entry(string prompt, string response)
+    {
+        _prompt = prompt;
+        _response = response;
+        _date = DateTime.Now;
+    }
+
+    public override string ToString()
+    {
+        return $"Date: {_date}\nPrompt: {_prompt}\nResponse: {_response}\n";
+    }
+}
+
+class Journal
+{
+    private List<Entry> entries = new List<Entry>();
+
+    public void AddEntry(Entry entry)
+    {
+        entries.Add(entry);
+    }
+
+    public void DisplayEntries()
+    {
+        foreach (var entry in entries)
+        {
+            Console.WriteLine(entry);
+        }
+    }
+
+    public void SaveToFile(string fileName)
+    {
+        using (StreamWriter writer = new StreamWriter(fileName))
+        {
+            foreach (var entry in entries)
+            {
+                writer.WriteLine($"{entry._date}\n{entry._prompt}\n{entry._response}\n");
+            }
+        }
+    }
+
+    public void LoadFromFile(string fileName)
+    {
+        entries.Clear();
+        using (StreamReader reader = new StreamReader(fileName))
+        {
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                string prompt = line;
+                string response = reader.ReadLine();
+                DateTime date = DateTime.Parse(reader.ReadLine());
+                entries.Add(new Entry(prompt, response) { _date = date });
+            }
+        }
+    }
+}
+
+class GeneratePrompt
+{
+    private static readonly string[] prompts = {
+        "Who was the most interesting person I interacted with today?",
+        "What was the best part of my day?",
+        "How did I see the hand of the Lord in my life today?",
+        "What was the strongest emotion I felt today?",
+        "If I had one thing I could do over today, what would it be?"
+    };
+
+    public static string GetRandomPrompt()
+    {
+        Random random = new Random();
+        int index = random.Next(prompts.Length);
+        return prompts[index];
+    }
+}
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Journal journal = new Journal();
+
+        while (true)
+        {
+            Console.WriteLine("1. Write a new entry");
+            Console.WriteLine("2. Display the journal");
+            Console.WriteLine("3. Save the journal to a file");
+            Console.WriteLine("4. Load the journal from a file");
+            Console.WriteLine("5. Exit");
+
+            Console.Write("Enter your choice: ");
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    string prompt = GeneratePrompt.GetRandomPrompt();
+                    Console.WriteLine("Prompt: " + prompt);
+                    Console.Write("Response: ");
+                    string response = Console.ReadLine();
+                    journal.AddEntry(new Entry(prompt, response));
+                    break;
+                case "2":
+                    journal.DisplayEntries();
+                    break;
+                case "3":
+                    Console.Write("Enter a filename to save the journal: ");
+                    string saveFileName = Console.ReadLine();
+                    journal.SaveToFile(saveFileName);
+                    break;
+                case "4":
+                    Console.Write("Enter a filename to load the journal: ");
+                    string loadFileName = Console.ReadLine();
+                    journal.LoadFromFile(loadFileName);
+                    break;
+                case "5":
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice. Please try again.");
+                    break;
+            }
+        }
     }
 }
+
